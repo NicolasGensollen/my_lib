cbae6b3eaff4517e9b5052faaf2a62eb467c4def
NicolasGensollen <nicolas.gensollen@gmail.com>
2024-09-09 14:14:36 +0200

    Implement addition, add numpy dependency, and lock

--- src/my_lib/__init__.py
+++ src/my_lib/__init__.py
@@ -1,2 +1,3 @@
-def hello() -> str:
-    return "Hello from my-lib!"
+from ._add import add
+
+__all__ = ["add"]
---
+++ src/my_lib/_add.py
@@ -0,0 +1,7 @@
+
+__all__ = ["add"]
+
+
+def add(a: int, b: int) -> int:
+    """Perform addition between provided arguments."""
+    return a + b
