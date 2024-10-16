e23700fe3393904d52eab063c5b42acf78770d02
NicolasGensollen <nicolas.gensollen@gmail.com>
2024-10-16 17:20:39 +0200

    Release 0.0.2

--- src/my_lib_gens/__init__.py
+++ src/my_lib_gens/__init__.py
@@ -2,6 +2,6 @@ from my_lib_gens._add import add
 from my_lib_gens._sub import sub
 from my_lib_gens._multiply import mult
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["add", "sub", "mult"]
