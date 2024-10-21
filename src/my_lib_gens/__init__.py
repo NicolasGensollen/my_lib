795b2dd62f19c94217c8f0605eef053bd4c0dc1f
Nicolas Gensollen <nicolas.gensollen@gmail.com>
2024-10-21 08:53:19 +0000

    NEW: Add `MyLibZeroDivisionError`

--- src/my_lib_gens/__init__.py
+++ src/my_lib_gens/__init__.py
@@ -3,7 +3,8 @@ from my_lib_gens._sub import sub
 from my_lib_gens._multiply import mult
 from my_lib_gens._divide import div
 from my_lib_gens._modulo import mod
+from my_lib_gens._errors import MyLibZeroDivisionError
 
 __version__ = "0.0.6"
 
-__all__ = ["add", "sub", "mult", "div", "mod"]
+__all__ = ["add", "sub", "mult", "div", "mod", "MyLibZeroDivisionError"]
--- src/my_lib_gens/_divide.py
+++ src/my_lib_gens/_divide.py
@@ -5,4 +5,8 @@ __all__ = ["div"]
 
 def div(a: float, b: float) -> float:
     """Perform division between provided arguments."""
+    from ._errors import MyLibZeroDivisionError
+
+    if b == 0:
+        raise MyLibZeroDivisionError("Cannot divide by zero")
     return a / b
